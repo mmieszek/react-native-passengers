9b95b560106d232257f4bc46b1015fdcbcb6822e
Marcin Mieszek <marcin.mieszek@gmail.com>
2019-02-25 23:00:23 +0100

    Display passenger avatar witb border

--- app/components/PassengerItem.js
+++ app/components/PassengerItem.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components/native'
+import Avatar from './Avatar'
 
 const PassengerItem = ({ firstName, lastName }) => (
   <Container>
+    <Avatar />
     <Info>
       {firstName} {lastName}
     </Info>
   </Container>
 )
-const Container = styled.View``
+const Container = styled.View`
+  flex-direction: row;
+`
 const Info = styled.Text``
 PassengerItem.propTypes = {
   firstName: PropTypes.string.isRequired,
