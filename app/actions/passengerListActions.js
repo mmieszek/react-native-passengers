c7cad046a77c4142d910ca0315b1c2651032f398
Marcin Mieszek <marcin.mieszek@gmail.com>
2019-02-26 13:08:06 +0100

    Use savePassenger for first passenger

--- app/actions/passengerListActions.js
+++ app/actions/passengerListActions.js
@@ -2,30 +2,25 @@ import { Alert } from 'react-native'
 import * as ActionTypes from './actionTypes'
 
 export function savePassenger(type, passenger) {
+  const avatar = Math.floor(Math.random() * 10)
+  const color = `rgb(${Math.floor(Math.random() * 266)}, ${Math.floor(
+    Math.random() * 266,
+  )}, ${Math.floor(Math.random() * 266)})`
   return {
     type: ActionTypes.SAVE_PASSENGER,
-    payload: { type, passenger },
+    payload: { type, passenger, avatar, color },
   }
 }
-export function firstPassengerFetched(firstPassenger) {
-  return {
-    type: ActionTypes.FIRST_PASSENGER_FETCHED,
-    payload: firstPassenger,
-  }
-}
-
 export function fetchStarted() {
   return {
     type: ActionTypes.FETCH_STARTED,
   }
 }
-
 export function fetchEnded() {
   return {
     type: ActionTypes.FETCH_ENDED,
   }
 }
-
 export function fetchFirstPassenger() {
   // eslint-disable-next-line consistent-return
   return async dispatch => {
@@ -35,7 +30,7 @@ export function fetchFirstPassenger() {
         'https://functionapp20180527095701.azurewebsites.net/api/GetUserTravellerInfo',
       )
       const firstPassenger = await response.json()
-      dispatch(firstPassengerFetched(firstPassenger))
+      dispatch(savePassenger('first', firstPassenger))
       dispatch(fetchEnded())
     } catch (error) {
       Alert.alert('Alert while fetching first passenger', error.message)
--- app/reducers/passengers.js
+++ app/reducers/passengers.js
@@ -7,24 +7,14 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.FIRST_PASSENGER_FETCHED: {
-      return {
-        ...state,
-        first: {
-          ...action.payload,
-          avatar: Math.floor(Math.random() * 10),
-          color: `rgb(${Math.floor(Math.random() * 266)}, ${Math.floor(
-            Math.random() * 266,
-          )}, ${Math.floor(Math.random() * 266)})`,
-        },
-      }
-    }
     case ActionTypes.SAVE_PASSENGER: {
+      const { type, passenger, color, avatar } = action.payload
       return {
         ...state,
-        additional: {
-          ...state.additional,
-          [action.payload.type]: action.payload.passenger,
+        [type]: {
+          ...passenger,
+          color,
+          avatar,
         },
       }
     }
